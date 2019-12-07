2bcebb861dcbb042346d81be524a58011eea2a79
Adrian Statescu <mergesortv@gmail.com>
2019-12-07 22:47:33 +0200

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <fstream>
+#define STACK_MAX 100
+#define FIN "scarface.in"
+
+using namespace std;
+
+class Stack {
+
+	  private:
+	  	      int size;
+	  	      
+	  	      int data[STACK_MAX];
+
+	  public:
+	  	     Stack() { //constructor
+                  
+                  size = 0;
+	  	     } 
+
+	  	     ~Stack() { //destructor
+
+	  	     }
+
+	  	     void Push(int v) {
+
+                  if(size < STACK_MAX) {
+
+                     data[size++] = v;             
+
+                  } else {
+
+                  	fprintf(stderr, "%s\n", "Error: Stack Overflow!");
+                  }
+	  	     }
+
+	  	     void Pop() {
+
+                  if(size == 0) {
+
+                  	 fprintf(stderr, "%s\n", "Error: Stack Underflow!");
+
+                  } else size--;
+	  	     }
+
+	  	     int Top() {
+
+                   if(size == 0) {
+
+                          fprintf(stderr, "%s\n", "Error: Stack is Empty!");
+
+                   } else return data[size - 1];
+
+	  	     }
+
+};
+
+void reverse(int *p, int n) {
+
+	Stack S;
+
+    for (int i = 0; i < n; ++i)
+	{
+		 S.Push(p[i]);
+	}
+	
+	for (int i = 0; i < n; ++i)
+	{
+		 p[i] = S.Top();
+
+		 S.Pop();
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+
+    int n, *p, i;    
+
+	freopen(FIN, "r", stdin);
+
+	cin>>n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin>>p[i];
+	}
+	
+    for (int i = 0; i < n; ++i) printf("%d ", p[i]);
+
+    reverse(p, n); 
+
+    printf("\n");
+	
+	for (int i = 0; i < n; ++i) printf("%d ", p[i]);
+	
+	return 0;
+}
